20f4e32d53caa99297419fcfb5b4a00327f5a8cf
andi <herlandi.27@gmail.com>
2020-03-29 18:21:36 +0700

    ganti data indonesia dan plot grafik

--- script/1-digest-data.R
+++ script/1-digest-data.R
@@ -1,15 +1,15 @@
-# digest data = načtení + denormalizace dat
+# digest data = memuat + denormalisasi data
 
 library(tidyverse)
 
 curl::curl_download(
   url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
-  destfile = "./data/time_series_19-covid-Confirmed.csv"
+  destfile = "./data/time_series_covid19-confirmed.csv"
 )
 
-raw_data <- readr::read_csv("./data/time_series_19-covid-Confirmed.csv") %>%
-  select(-"Province/State", -Lat, -Long, zeme = "Country/Region") %>%
-  pivot_longer(-zeme, names_to = "datum", values_to = "pocet") %>%
-  mutate(datum = lubridate::mdy(datum))
+raw_data <- readr::read_csv("./data/time_series_covid19-confirmed.csv") %>%
+  select(-"Province/State", -Lat, -Long, negara = "Country/Region") %>%
+  pivot_longer(-negara, names_to = "tanggal", values_to = "jumlah") %>%
+  mutate(tanggal = lubridate::mdy(tanggal))
 
 write_csv2(raw_data, "./data/raw_data.csv")
--- script/2-draw-chart.R
+++ script/2-draw-chart.R
@@ -1,72 +1,70 @@
-# vytvořit a uložit graf jako obrázek
+# membuat dan menyimpan grafik sebagai gambar
 
 library(tidyverse)
 
-pdf.options(encoding = "ISOLatin2") # aby nepadal pandoc na konverzi češtiny v nadpisech grafů
-
 clean_data <- read_csv2("./data/raw_data.csv") %>%
-  filter(zeme == "Czechia" & pocet > 0) %>%
-  mutate(den = lubridate::day(datum))
+  filter(negara == "Indonesia" & jumlah > 0) %>%
+  mutate(hari = lubridate::day(tanggal))
 
-trend <- nls(pocet ~ a * (1 + r)^(den),
-  data = subset(clean_data, den < 21),
+
+# PR: cari trend yang sesuai untuk data Indonesia
+trend <- nls(jumlah ~ a * (1 + r)^(hari),
+  data = subset(clean_data, hari < 29),
   start = list(a = 1, r = .01)
 )
 
-budoucnost <- data.frame(den = 21:31)
+besok <- data.frame(hari = 29:41)
 
-predpoved <- data.frame(
-  datum = c(as.Date("2020-03-21"):as.Date("2020-03-31")) %>% as.Date(origin = as.Date("1970-01-01")),
-  pocet = predict(trend, newdata = budoucnost)
+prediksi <- data.frame(
+  tanggal = c(as.Date("2020-03-29"):as.Date("2020-04-10")) %>% as.Date(origin = as.Date("1970-01-01")),
+  jumlah = predict(trend, newdata = besok)
 )
 
-ggplot(data = clean_data, aes(x = datum, y = pocet)) +
+# plot data
+ggplot(data = clean_data, aes(x = tanggal, y = jumlah)) +
   geom_text(
-    data = predpoved, aes(
-      x = datum, y = pocet,
-      label = formatC(pocet, big.mark = " ", format = "f", digits = 0)
-    ),
+    data = prediksi, 
+    mapping = aes(
+      x = tanggal, y = jumlah,
+      label = formatC(jumlah, big.mark = ".", decimal.mark = ",", 
+        format = "f", digits = 0)),
     hjust = 1.2,
-    color = "gray50"
-  ) +
-  geom_smooth(data = filter(clean_data, datum >= as.Date("2020-03-21")),
-              aes(color = "gray75"),
-              method = "lm", size = .5, fullrange = T, se = F, linetype = "dashed") +
-  geom_line(aes(color = "firebrick"), lwd = 1.2) +
-  geom_point(data = predpoved, aes(
-    x = datum, y = pocet), pch = 4) +
-  geom_text(data = slice(clean_data, which.max(datum)), aes(
-    x = datum, y = pocet,
-    label = formatC(pocet, big.mark = " ", format = "f", digits = 0)
-  ), hjust = -.5, color = "firebrick") +
+    color = "gray50") +
+  geom_smooth(
+    data = filter(clean_data, tanggal >= as.Date("2020-03-23")), 
+    aes(color = "gray75"), 
+    method = "lm", 
+    size = .5, fullrange = T, se = F, linetype = "dashed") +
+  geom_line(color = "firebrick", lwd = 1.2) +
+  geom_point(data = prediksi, pch = 4) +
+  geom_text(
+    data = slice(clean_data, which.max(tanggal)),
+    mapping = aes(
+      x = tanggal, y = jumlah, 
+      label = formatC(jumlah, big.mark = ".", decimal.mark = ",", 
+                      format = "f", digits = 0)), 
+    hjust = -.5, color = "firebrick") +
   labs(
-    title = "Trend šíření nákazy COVID-19 v ČR",
-    color = "Počet nakažených",
-    caption = paste("zdroj dat: Johns Hopkins, stav k", max(clean_data$datum) %>%
-      format(format = "%d.%m.%Y"))
-  ) +
+    title = "Kecenderungan infeksi COVID-19 di Indonesia", 
+    color = "Jumlah terinfeksi", 
+    caption = paste("Sumber data: Johns Hopkins per", 
+      max(clean_data$tanggal) %>% format(format = "%d-%m-%Y"))) +
   scale_x_date(
-    date_breaks = "1 day",
-    minor_breaks = NULL,
-    labels = scales::date_format(format = "%d.%m."),
-    limits = as.Date(c("2020-03-02", "2020-03-30"))
-  ) +
+    date_breaks = "1 day", 
+    minor_breaks = NULL, 
+    labels = scales::date_format(format = "%d-%m"), 
+    limits = as.Date(c("2020-03-10", "2020-04-14"))) +
   scale_y_log10(labels = scales::number_format()) +
   scale_color_identity(
-    labels = c("skutečnost", "trend od 20.3.2020"),
-    guide = guide_legend(
-      title.position = "top",
-      title.hjust = 0.5)
-    ) +
+    labels = c("fakta", "tren dari 20-03-2020"), 
+    guide = guide_legend(title.position = "top", title.hjust = 0.5)) +
   theme_linedraw() +
   theme(
     axis.text.x = element_text(angle = 90),
     axis.title = element_blank(),
     panel.grid.major = element_line(color = "gray75"),
     plot.caption = element_text(color = "gray25"),
-    legend.position = "bottom"
-  )
+    legend.position = "bottom")
 
-
-ggsave("./img/obrazek.pdf", dpi = 300, units = "cm", width = 25, height = 20)
-ggsave("./img/obrazek.png", dpi = 300, units = "cm", width = 25, height = 20)
+ggsave("./img/gambar.pdf", dpi = 150, units = "cm", width = 25, height = 20)
+ggsave("./img/gambar.png", dpi = 150, units = "cm", width = 25, height = 20)
