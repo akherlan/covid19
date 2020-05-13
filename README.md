10f1dab423e4d0e9e4caeebb587d4c5a6049bdc9
Andi Herlan <herlandi.27@gmail.com>
2020-05-14 03:53:34 +0700

    setelah sekian lama tidak update

--- README.md
+++ README.md
@@ -4,18 +4,19 @@ _Repositori ini merupakan hasil klon dari_ [_analisis koronavirus di Ceko_](http
 
 Representasi grafis dari kasus penyebaran koronavirus baru (nCoV-19) di Indonesia. Sumber data dari basis data yang terkenal di dunia saat ini yaitu Universitas John Hopkins, tersedia di GitHub: https://github.com/CSSEGISandData/COVID-19
 
-Fakta jumlah kasus sesuai dengan data yang dilaporkan per 1 April 2020. Model sesuai dengan tren sejak 25 Maret 2020 (sepekan terakhir). Jumlah kasus lebih rendah dari prediksi.
-
-<p align="center">
-  <img src="./img/image.png" alt="trendline"/>
-</p>
-
-Kita berharap grafik akan terus mendatar (semendatar mungkin), berada di bawah titik-titik (* * *) prediksi, yang berarti kita menang melawan koronavirus.
-
-Sebagai pembanding diberikan pula grafik jumlah kasus Covid-19 dari negara lain di dunia. Kita dapat melihat bagaimana peningkatan kasus secara eksponensial mulai berhenti di Tiongkok dan Korea Selatan, melambat di Jepang, Singapura, dan Malaysia, namun tetap bertahan naik di Italia, Iran, dan Amerika Serikat, juga Inggris.
-
 <p align="center">
   <img src="./img/foreign.png" alt="foreign"/>
 </p>
 
-_Dimutakhirkan pada 2 April 2020_
+Diberikan grafik jumlah kasus Covid-19 di Indonesia dibandngkan dengan negara-negara lain di dunia. Kita dapat melihat bahwa:
+
+- kasus Covid-19 di Tiongkok sudah mereda,
+- Singapura dan Jepang mempunyai grafik yang unik dan berbeda dari yang lain, kita perlu melihat bagaimana usaha kedua negara ini membendung pandemi,
+- secara eksponensial mulai berhenti di Korea Selatan, Malaysia, dan Italia,
+- namun masih tetap bertahan naik di Indonesia, Iran, Amerika Serikat, dan Inggris, juga Singapura yangmana bersifar fluktuatif seperti Jepang.
+
+_Dimutakhirkan pada 14 Mei 2020, dini hari, waktu Indonesia bagian Barat_
+
+**N.B.**
+
+Saya telah merusak kode untuk prediksi. Perlu belajar lebih lanjut.
--- script/2-draw-chart.R
+++ script/2-draw-chart.R
@@ -8,13 +8,14 @@ clean_data <- read_csv2("./data/raw_data.csv") %>%
 
 # berapa seharusnya nilai a dan r yang tepat?
 trend <- nls(jumlah ~ a * (1 + r)^(hari),
-  data = subset(clean_data, hari < 26),
+  data = subset(clean_data, nrow(clean_data) < nrow(clean_data)-14),
   start = list(a = 1, r = .01))
 
-besok <- data.frame(hari = 26:36)
+besok <- data.frame(hari = (nrow(clean_data)-14):(nrow(clean_data)+1))
 
 prediksi <- data.frame(
-  tanggal = c(as.Date("2020-03-26"):as.Date("2020-04-05")) %>% as.Date(origin = as.Date("1970-01-01")),
+  tanggal = c(as.Date("2020-04-03"):max(clean_data$tanggal)) %>%
+    as.Date(origin = as.Date("1970-01-01")),
   jumlah = predict(trend, newdata = besok))
 
 ggplot(data = clean_data, aes(x = tanggal, y = jumlah)) +
@@ -28,7 +29,7 @@ ggplot(data = clean_data, aes(x = tanggal, y = jumlah)) +
     hjust = 1.3,
     color = "gray50") +
   geom_smooth(
-    data = filter(clean_data, tanggal >= as.Date("2020-03-25")), 
+    data = filter(clean_data, tanggal >= as.Date("2020-04-10")), 
     aes(color = "gray50"), 
     method = "lm", 
     size = .4, fullrange = T, se = F, linetype = "dashed") +
@@ -43,17 +44,22 @@ ggplot(data = clean_data, aes(x = tanggal, y = jumlah)) +
     hjust = -.5, color = "firebrick", size = 4) +
   labs(
     title = "Kecenderungan infeksi COVID-19 di Indonesia", 
-    subtitle = "Prediksi * * * sejak 26 Maret 2020 ",
+    subtitle = "Prediksi * * * sejak 2 pekan terakhir",
     caption = paste("Sumber data: Johns Hopkins per", 
       max(clean_data$tanggal) %>% format(format = "%d %b %Y"))) +
   scale_x_date(
     date_breaks = "1 day", 
     minor_breaks = NULL, 
     labels = scales::date_format(format = "%d/%m"), 
-    limits = as.Date(c("2020-03-10", "2020-04-06"))) +
+    limits = as.Date(c("2020-03-10", "2020-04-25"))) +
   scale_y_log10(labels = scales::number_format()) +
   scale_color_identity(
-    labels = c("jumlah kasus", "tren dalam satu pekan terakhir (sejak 25 Mar 2020)"), 
+    labels = c("jumlah kasus", 
+               paste0("tren dalam satu pekan terakhir (sejak ", 
+                      as.character(
+                        format.Date(head(tail(clean_data, 7), 1)$tanggal,
+                                    format = "%d %b %Y")), 
+                      ")")), 
     guide = guide_legend(title.position = "bottom", title.hjust = 0.5)) +
   theme_linedraw() +
   theme(
@@ -64,5 +70,4 @@ ggplot(data = clean_data, aes(x = tanggal, y = jumlah)) +
     legend.title = element_blank(),
     legend.position = "bottom")
 
-ggsave("./img/image.pdf", dpi = 300, units = "cm", width = 25, height = 20)
 ggsave("./img/image.png", dpi = 300, units = "cm", width = 25, height = 20)
